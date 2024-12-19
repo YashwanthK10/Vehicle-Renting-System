d11aa03df2a0e28c8c04b2d4ded0b2533b7d73bd
YashwanthK10 <yashwanth26102002@gmail.com>
2024-12-19 13:43:33 +0530

    Performing the vehicle images upload operation

--- src/main/java/com/example/vms/service/ImageService.java
+++ src/main/java/com/example/vms/service/ImageService.java
@@ -1,5 +1,7 @@
 package com.example.vms.service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.stereotype.Service;
@@ -7,22 +9,26 @@ import org.springframework.web.multipart.MultipartFile;
 
 import com.example.vms.entity.Image;
 import com.example.vms.entity.User;
+import com.example.vms.entity.Vehicle;
 import com.example.vms.exception.FailedToUploadException;
 import com.example.vms.exception.ImageNotFoundById;
 import com.example.vms.exception.UserNotFoundException;
 import com.example.vms.repository.ImageRepository;
 import com.example.vms.repository.UserRepository;
+import com.example.vms.repository.VehicleRepository;
 
 @Service
 public class ImageService {
 	
 	private final ImageRepository imageRepository;
-	private final UserRepository userRepository; 
+	private final UserRepository userRepository;
+	private final VehicleRepository vehicleRepository;
 
-	public ImageService(ImageRepository imageRepository, UserRepository userRepository) {
+	public ImageService(ImageRepository imageRepository, UserRepository userRepository, VehicleRepository vehicleRepository) {
 		super();
 		this.imageRepository = imageRepository;
 		this.userRepository = userRepository;
+		this.vehicleRepository = vehicleRepository;
 	}
 
 	public void uploadPicture(int userId, MultipartFile file) {
@@ -77,6 +83,23 @@ public class ImageService {
 		else {
 			throw new ImageNotFoundById("Image is not there in database");
 		}
+	}
+
+	public void uploadVehicleImages(int vehicleId, List<MultipartFile> files) {
+		
+		Vehicle vehicle = vehicleRepository.findById(vehicleId)
+				.orElseThrow(() -> new UserNotFoundException("Vehicle not found with ID: " + vehicleId));
+
+		List<Image> images = new ArrayList();
+
+		for (MultipartFile file : files) {
+			images.add(this.getImage(file));
+		}
+
+		images = imageRepository.saveAll(images);
+		vehicle.setImages(images);
+		vehicleRepository.save(vehicle);
+		
 	}	
 	
 	
